07225ab526cd61648e64a2dc25c0d3cb6b1c0998
daFoggo <ntgiang141105@gmail.com>
2024-03-22 18:15:58 +0700

    account profile ui

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom'
 
 const App = () => {
     return (
-        <div className='App bg-gradient-to-tr from-[#f0f7ff] to-[#e0eefe] flex w-screen h-screen text-[#172754]'>
+        <div className='App bg-gradient-to-r from-[#f0f7ff] to-[#a0c0fc] flex w-screen h-screen text-[#172754]'>
             <NavBar></NavBar>
             <div className='bg-[white] flex flex-col flex-auto mt-3 mr-3 ml-16 rounded-2xl p-5 shadow-md'>
                 <Header></Header>
